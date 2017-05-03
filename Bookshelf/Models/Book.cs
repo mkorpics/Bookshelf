c480c4f1728a812dae19efc622e6aa09d9e2c8cf
mkorpics <megkorpics@gmail.com>
2017-05-03 14:52:48 -0400

    initial commit

---
+++ Bookshelf/Models/Book.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Bookshelf.Models
+{
+    public class Book
+    {
+        //Properties. Basically telling us what kind of data is going to be entered.
+
+        [Key]
+        public int ID { get; set; }
+        public string Title { get; set; }
+
+        [Display(Name = "Date Published")] 
+        [DisplayFormat(DataFormatString = "{0:yyyy}")]
+        public DateTime PublishedDate { get; set; }
+
+        public string Description { get; set; }
+        public string ISBN { get; set; }
+
+        [ForeignKey("Category")] //a primary key from a different table. Also only refernces line directly below it.
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+
+    }
+}
---
+++ Bookshelf/Models/Category.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Bookshelf.Models
+{
+    public class Category
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Display(Name = "Category Name")]
+        public string Name { get; set; }
+
+        public virtual ICollection<Book> Books { get; set; } 
+        //We have a collection of books because
+        //even though each book only has a single category (ex. Fantasy)
+        //each category (ex. Fantasy) has many books.
+        //Don't have to use an ICollection.
+        //We are using it here because it doesn't have as many features as a list
+        //and so is more efficieint in certain ways than a List.
+
+    }
+}
